e9bcc72fd18a3331ec1afdd81a228a6eefaa0e3d
renanbaggio <renan_baggio@hotmail.com>
2014-01-31 17:03:05 -0200

    Adicionado classes de utilidades e novos packages

--- jove-timesheet/src/br/com/jovetecnologia/infrastructure/util/Messages.java
+++ jove-timesheet/src/br/com/jovetecnologia/infrastructure/util/Messages.java
@@ -1,5 +1,26 @@
 package br.com.jovetecnologia.infrastructure.util;
 
-public class Messages {
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 
+import org.primefaces.context.RequestContext;
+
+public class Messages {
+	
+	public static void addInfo(String detail) {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "", detail));
+	}
+
+	public static void addWarn(String detail) {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "", detail));
+	}
+
+	public static void addError(String detail) {
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "", detail));
+	}
+
+	public static void addFatal(String detail) {
+		RequestContext.getCurrentInstance().addCallbackParam("businessError", true);
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "", detail));
+	}
 }
