96b1e9acbd45b350cf2518a9163b3dfffeeb1544
joaquimsn <jsn.piata@gmail.com>
2014-02-03 23:54:24 -0200

    Criação da tela de login e do template padrão

--- jove-timesheet/src/br/com/jovetecnologia/web/bean/LoginBean.java
+++ jove-timesheet/src/br/com/jovetecnologia/web/bean/LoginBean.java
@@ -1,5 +1,51 @@
 package br.com.jovetecnologia.web.bean;
 
-public class LoginBean {
+import java.io.Serializable;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.com.jovetecnologia.domain.model.Usuario;
+import br.com.jovetecnologia.infrastructure.util.DAOFactory;
+
+@ManagedBean
+@SessionScoped
+public class LoginBean implements Serializable {
+
+	private static final long serialVersionUID = 5185840091249342115L;
+
+	private Usuario usuarioSelecionado;
+	private DAOFactory daoFactory;
+
+	/**
+	 * Inicializa os objetos da class
+	 * @author Joaquim Neto
+	 */
+	public LoginBean() {
+		usuarioSelecionado = new Usuario();
+		daoFactory = new DAOFactory();
+	}
+
+	/**
+	 * Realiza o login verificando o usuário e a senha na base
+	 * @author Joaquim Neto
+	 */
+	public void fazLogin() {
+
+	}
+
+	/**
+	 * @return the usuarioSelecionado
+	 */
+	public Usuario getUsuarioSelecionado() {
+		return usuarioSelecionado;
+	}
+
+	/**
+	 * @param usuarioSelecionado the usuarioSelecionado to set
+	 */
+	public void setUsuarioSelecionado(Usuario usuarioSelecionado) {
+		this.usuarioSelecionado = usuarioSelecionado;
+	}
 
 }
