d70d3559a86a1ac1ef3cb35bc78cc50533c8b473
joaquimsn <jsn.piata@gmail.com>
2014-02-14 00:38:57 -0200

    Criada a tela de acompanhamento e as telas para include

---
+++ jove-timesheet/src/br/com/jovetecnologia/web/bean/AcompanhamentoBean.java
@@ -0,0 +1,95 @@
+package br.com.jovetecnologia.web.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import br.com.jovetecnologia.domain.model.Funcionario;
+import br.com.jovetecnologia.web.util.IOutcome;
+
+@ManagedBean
+@SessionScoped
+public class AcompanhamentoBean {
+	
+	private Funcionario funcionario;
+	private List<Funcionario> listaFuncionario;
+	
+	/**
+	 * Refazer esse metodo, só teste
+	 * @author Joaquim Neto
+	 */
+	public AcompanhamentoBean(){
+		listaFuncionario = new ArrayList<Funcionario>();
+		
+		String[] nomes =  {"Joaquim", "Nicolas", "Renan", "Neto", "Baggio", "Carlos", "Verissimo", "Pedro", "Marcos", "Maria" };
+		
+		for(int i = 0; i < 10; i++){
+			Funcionario funcionario1 = new Funcionario();
+			double aleatorio = Math.random() * 10;
+			
+			String numero = "" + aleatorio;
+			
+			funcionario1.setNome(nomes[i]);
+			funcionario1.setNumero(numero);
+			
+			listaFuncionario.add(funcionario);
+			
+		}
+		
+	}
+	
+	/**
+	 * Responsavel por instanciar os objetos e variaveis essenciais para pagina
+	 * 
+	 * @author Joaquim Neto
+	 * @return O nome da pagina solicitada
+	 */
+	public String abrirPagina(){
+		funcionario = new Funcionario();
+		listaFuncionario = new ArrayList<Funcionario>();
+		
+		return IOutcome.ACOMPANHAMENTO;
+	}
+	
+	public boolean hasFuncionarioSelecionado(){
+		
+		if (funcionario.getNome().equals("")){
+			return false;
+		}
+		
+		return true;
+	}
+
+	/**
+	 * @return the funcionario
+	 */
+	public Funcionario getFuncionario() {
+		return funcionario;
+	}
+
+	/**
+	 * @param funcionario the funcionario to set
+	 */
+	public void setFuncionario(Funcionario funcionario) {
+		this.funcionario = funcionario;
+	}
+
+	/**
+	 * @return the listaFuncionario
+	 */
+	public List<Funcionario> getListaFuncionario() {
+		return listaFuncionario;
+	}
+
+	/**
+	 * @param listaFuncionario the listaFuncionario to set
+	 */
+	public void setListaFuncionario(List<Funcionario> listaFuncionario) {
+		this.listaFuncionario = listaFuncionario;
+	}
+	
+
+}
