ec1db7e940edc575119b6fadb02f3ff50e831c20
Joaquim Neto <jsn.piata@gmail.com>
2014-04-22 10:36:55 -0300

    Ajuste

--- jove-timesheet/src/br/com/jovetecnologia/web/bean/RegistroAdministradorBean.java
+++ jove-timesheet/src/br/com/jovetecnologia/web/bean/RegistroAdministradorBean.java
@@ -6,7 +6,6 @@ import javax.faces.bean.ViewScoped;
 @ManagedBean
 @ViewScoped
 public class RegistroAdministradorBean extends RegistroBean {
-
 	private static final long serialVersionUID = -5558818337201761377L;
 
 }
